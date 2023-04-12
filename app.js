9ba0111329d66aca09f3d488ed940303c50f51a5
Sudeep4686 <sudeephipparge@gmail.com>
2023-04-12 21:43:44 +0530

    addingStyles

--- app.js
+++ app.js
@@ -1,31 +1,25 @@
+
 const http = require('http');
+const path = require('path');
+
 
 const express = require('express');
 const bodyParser = require('body-parser')
 
 const app = express();
 
-// app.use('/',(req,res,next)=>{
-//     console.log("This always works");
-//     next();
-// })
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,'public')));
 
-app.use('/add-product',(req,res,next)=>{
-    console.log("In the next middleware!");
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button><button type="submit">Add Size</button></form>');
-    // res.send('<form action="/product" method="POST"><input type="text" name="size"><button type="submit">Add Size</button></form>');
-});
 
-app.use('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 })
 
-app.use('/',(req,res,next)=>{
-
-    res.send('<h1>Hello from Express!</h1>');
-});
-
-app.listen(3000);
+app.listen(20000);
